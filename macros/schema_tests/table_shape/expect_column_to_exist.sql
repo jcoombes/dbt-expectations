640eee8c87eca4e0dcf8ec3766ed9c0bd71b1b2b
Jamie Coombes <coombes.jd@gmail.com>
2023-09-19 12:59:37 +0100

    quote for table existence

--- macros/schema_tests/table_shape/expect_column_to_exist.sql
+++ macros/schema_tests/table_shape/expect_column_to_exist.sql
@@ -1,30 +1,27 @@
 {%- test expect_column_to_exist(model, column_name, column_index=None, transform="upper") -%}
 {%- if execute -%}
 
-    {%- set column_name = column_name | map(transform) | join -%}
+    {%- set quote_column = model.config.quote -%}
+    
+    {%- set column_name_transformed = column_name | map(transform) | join -%}
+    {%- set quoted_column_name = (quote_column and '"' ~ column_name_transformed ~ '"') or column_name_transformed -%}
+    
     {%- set relation_column_names = dbt_expectations._get_column_list(model, transform) -%}
 
-    {%- set matching_column_index = relation_column_names.index(column_name) if column_name in relation_column_names else -1 %}
+    {%- set matching_column_index = relation_column_names.index(quoted_column_name) if quoted_column_name in relation_column_names else -1 %}
 
     {%- if column_index -%}
-
         {%- set column_index_0 = column_index - 1 if column_index > 0 else 0 -%}
-
         {%- set column_index_matches = true if matching_column_index == column_index_0 else false %}
-
     {%- else -%}
-
         {%- set column_index_matches = true -%}
-
     {%- endif %}
 
     with test_data as (
-
         select
-            cast('{{ column_name }}' as {{ dbt.type_string() }}) as column_name,
+            cast('{{ quoted_column_name }}' as {{ dbt.type_string() }}) as column_name,
             {{ matching_column_index }} as matching_column_index,
             {{ column_index_matches }} as column_index_matches
-
     )
     select *
     from test_data
